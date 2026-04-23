044f88b2840f26147b3c6933c1c360a3e3750909
rajat kumar <rajatko614@gmail.com>
2026-04-24 00:19:51 +0530

    [ADD]: config

---
+++ src/main/java/com/finance/conf/Config.java
@@ -0,0 +1,21 @@
+package com.finance.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Config {
+    @CrossOrigin(origins = "*")  // on your controller or method
+// or globally in config:
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+}
